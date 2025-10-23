d79de6bd1b13dd3486507132d3343f7fcc5b5d74
Merlin Raabe <merlinraabe@yahoo.de>
2025-10-23 19:21:44 +0200

    db and auth improvement

--- src/modules/authz/jwt.strategy.ts
+++ src/modules/authz/jwt.strategy.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
 import { passportJwtSecret } from 'jwks-rsa';
 import * as dotenv from 'dotenv';
 import { UsersService } from '../user/users.service'; // adjust path if needed
+
 import { User } from '../user/user.entity'; // adjust path if needed
 
 dotenv.config();
@@ -33,10 +34,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: JwtPayload): Promise<User> {
     const auth0Id = payload.sub;
-    let user = await this.usersService.findByAuth0Id(auth0Id);
-    if (!user) {
-      user = await this.usersService.create(auth0Id);
+    Logger.log('JWT payload:', payload);
+    const user = await this.usersService.findByAuth0Id(auth0Id);
+    if (!!user) {
+      Logger.log(`found user: ${user.auth0_id}`, 'JwtStrategy');
+      return user;
     }
-    return user;
+
+    Logger.log('creating user for auth0Id:', auth0Id);
+    const newUser = await this.usersService.create(auth0Id);
+    return newUser;
   }
 }
